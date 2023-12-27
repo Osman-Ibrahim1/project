9ba1b0f62147087fdaf843425caf5aea9d6bb0c7
Robinlesss <150183018+Robinlesss@users.noreply.github.com>
2023-12-27 23:55:05 +0100

    Nu har jag lagt till kod igen!!

--- project/Program.cs
+++ project/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Osman!");
             Console.WriteLine("senap");
+            Console.WriteLine("fdedjnjn ");
         }
     }
 }
