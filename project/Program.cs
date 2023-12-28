886a6d8d25e8ac54da6ffa32dab2bb86c09ad5e1
Osman Adam Ibrahim <h23osmib@du.se>
2023-12-28 13:16:01 +0100

    osmans test

--- project/Program.cs
+++ project/Program.cs
@@ -4,10 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Osman!");
-            Console.WriteLine("Osman!");
-            Console.WriteLine("bos!");
+            while (true)
+            {
+                // Användarens input
+                Console.Write("Ange det första talet: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ange operatören (+, -, *, /): ");
+                string op = Console.ReadLine();
+
+                Console.Write("Ange det andra talet: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                // Utför beräkningen beroende på operatören
+                double result = 0;
+                switch (op)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        // Kontrollera så att man inte dividerar med noll
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel: Kan inte dividera med noll.");
+                            continue;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Felaktig operatör.");
+                        continue;
+                }
+
+                // Visa resultatet
+                Console.WriteLine("Resultat: " + result);
+
+                // Fråga om användaren vill fortsätta
+                Console.Write("Vill du göra en ny beräkning? (ja/nej): ");
+                string continueInput = Console.ReadLine().ToLower();
+
+                if (continueInput != "ja")
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
