de52e4cf2c3f4ef1a04f341b6908d034b64b4d2e
Osman-Ibrahim1 <150298764+Osman-Ibrahim1@users.noreply.github.com>
2024-01-06 01:40:23 +0100

    la till klasser

---
+++ project/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+         internal class Miniräknare
+    {
+        public double Addera(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtrahera(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplicera(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Dividera(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new ArgumentException("Kan inte dividera med noll.");
+            }
+
+            return num1 / num2;
+        }
+    }
+}
+}
